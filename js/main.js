b5258176ccb2f9fef87eeff18ff5bb7539025279
Mission Bit <russell.s.low@gmail.com>
2018-05-03 16:01:02 -0700

    added 3 levels

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ Spider.prototype.die = function () {
 
 PlayState = {};
 
-const LEVEL_COUNT = 2;
+const LEVEL_COUNT = 3;
 
 PlayState.init = function (data) {
     this.game.renderer.renderSession.roundPixels = true;
@@ -154,7 +154,7 @@ PlayState.init = function (data) {
 PlayState.preload = function () {
     this.game.load.json('level:0', 'data/level00.json');
     this.game.load.json('level:1', 'data/level01.json');
-//    this.game.load.json('level:2', 'data/level02.json');
+    this.game.load.json('level:2', 'data/level02.json');
 
     this.game.load.image('font:numbers', 'images/numbers.png');
     this.game.load.image('background', 'images/background.png');
